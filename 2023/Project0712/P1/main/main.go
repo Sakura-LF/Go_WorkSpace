8122721e2a3ec76d5df4d0947da5e65ac5b1b12b
Sakura <sakuralfszj@gmail.com>
2023-07-13 13:53:00 +0800

    目录结构调整

--- 2023/Project0712/P1/main/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("sakura")
-	fmt.Println("sakura")
-}
--- Gin/230713/P1/main/main.go
+++ Gin/230713/P1/main/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("12341")
+	fmt.Println("Test")
 }
