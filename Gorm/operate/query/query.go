45522d85f9bfc49cfceff96109f083e3a81073ba
Sakura <sakuralfszj@gmail.com>
2023-07-23 14:14:48 +0800

    2023.7.23 Gorm学习: 1. 查询多条 2. 查询结果到Map映射 3. 查询单列 4. Select字段 5. distinct字段

--- Gorm/operate/query/query.go
+++ Gorm/operate/query/query.go
@@ -1,7 +1,9 @@
 package query
 
 import (
+	"database/sql"
 	"errors"
+	"fmt"
 	"gorm.io/gorm"
 	"log"
 	"time"
@@ -21,6 +23,10 @@ type Content struct {
 	Likes       uint
 	Views       uint
 	PublishTime *time.Time
+
+	// 不需要迁移
+	// 禁用写操作
+	SV string `gorm:"-migration;<-:false>"`
 }
 
 func GetPrimaryKey() {
@@ -64,4 +70,146 @@ func GetPrimaryKey() {
 			log.Fatal(err)
 		}
 	}
+}
+
+func QueryOne() {
+	c := Content{}
+	if err := DB.First(&c, "id > ?", 42).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println("Record Not Foud")
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	o := Content{}
+	if err := DB.Last(&o, "id > ?", 42).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println("Record Not Foud")
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	n := Content{}
+	if err := DB.Take(&n, "id > ?", 42).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println("Record Not Foud")
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	f := Content{}
+	if err := DB.Limit(1).Find(&f, "id > ?", 42).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println("Record Not Foud")
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	fs := Content{}
+	if err := DB.Find(&fs, "id > ?", 42).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println("Record Not Foud")
+		} else {
+			log.Fatal(err)
+		}
+	}
+}
+
+func QueryToMap() {
+	//查询单条
+	One := map[string]any{}
+	if err := DB.Model(&Content{}).First(&One, 13).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println("Record Not Found")
+		} else {
+			log.Fatal(err)
+		}
+	}
+	fmt.Println(One["id"], One["id"] == 13)
+	//value是any类型,使用时需要类型断言
+	if One["id"].(uint) == 13 {
+		fmt.Println("Test Success")
+	}
+	fmt.Println(One)
+
+	//查询多条
+	Many := []map[string]any{}
+	if err := DB.Model(&Content{}).Find(&Many, []uint{13, 14, 15}).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println("Record Not Found")
+		} else {
+			log.Fatal(err)
+		}
+	}
+	for _, value := range Many {
+		fmt.Println(value)
+	}
+}
+
+func QueryPluck() {
+	//使用切片存储
+	Subjects := []sql.NullString{}
+	if err := DB.Model(&Content{}).Pluck("Subject", &Subjects).Error; err != nil {
+		log.Fatal(err)
+	}
+	for _, Subjects := range Subjects {
+		//NullString的使用
+		//Subject.Valid是布尔类型,表示是否为空字符串
+		//true:表示不为空
+		//false:表示为空
+		if Subjects.Valid {
+			fmt.Println(Subjects.String)
+		} else {
+			fmt.Println("NULL")
+		}
+	}
+}
+
+func QueryPluckEXP() {
+	//使用切片存储
+	Subjects := []sql.NullString{}
+	//Pluck中的column也可以用表达式
+	if err := DB.Model(&Content{}).Pluck("concat(subject,'-',likes)", &Subjects).Error; err != nil {
+		log.Fatal(err)
+	}
+	for _, Subjects := range Subjects {
+		//NullString的使用
+		//Subject.Valid是布尔类型,表示是否为空字符串
+		//true:表示不为空
+		//false:表示为空
+		if Subjects.Valid {
+			fmt.Println(Subjects.String)
+		} else {
+			fmt.Println("NULL")
+		}
+	}
+}
+
+func QuerySelect() {
+	c := Content{}
+	if err := DB.Select("subject", "likes").First(&c, "14").Error; err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v", c)
+}
+
+func QuerySelectEXP() {
+	c := Content{}
+	if err := DB.Select("subject", "likes", "concat(subject,'-',likes) AS sv").First(&c, "14").Error; err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v", c)
+}
+
+func QueryDistinct() {
+	c := Content{}
+
+	if err := DB.Distinct("*").First(&c, 14).Error; err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%-v", c)
 }
--- Gorm/operate/query/query_test.go
+++ Gorm/operate/query/query_test.go
@@ -5,3 +5,27 @@ import "testing"
 func TestGetPrimaryKey(t *testing.T) {
 	GetPrimaryKey()
 }
+
+func TestQueryOne(t *testing.T) {
+	QueryOne()
+}
+
+func TestQueryToMap(t *testing.T) {
+	QueryToMap()
+}
+
+func TestQueryPluck(t *testing.T) {
+	QueryPluck()
+}
+
+func TestQuerySelect(t *testing.T) {
+	QuerySelect()
+}
+
+func TestQuerySelectEXP(t *testing.T) {
+	QuerySelectEXP()
+}
+
+func TestQueryDistinct(t *testing.T) {
+	QueryDistinct()
+}
